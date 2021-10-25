08d9931fe92dce9f1e8d651e380de7d1196dbeb4
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-10-25 15:51:13 +0300

    added cycle for, string conversion from camelCase to snake_case

--- src/laboratoryWorkString/Task1.java
+++ src/laboratoryWorkString/Task1.java
@@ -5,10 +5,15 @@ package laboratoryWorkString;
 
 import laboratoryWorkString.util.DemoStrings;
 
-public class Task1 { //FIXME
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+public class Task1 {
     public static void main(String[] args) {
         for (int i = 0; i < DemoStrings.testLinesArray.length; i++) {
-            String testLine = DemoStrings.testLinesArray[i];
+            String testLine = DemoStrings.testLinesArray[i].replaceAll("([a-z])([A-Z])",
+                    "$1_$2");
+            System.out.println(testLine.toLowerCase());
         }
     }
 }
