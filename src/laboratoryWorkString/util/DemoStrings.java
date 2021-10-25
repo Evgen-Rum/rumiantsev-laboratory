828a53c535c8a90541b39eec9dc72180ee43e128
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-10-25 15:57:23 +0300

    fixed line in String massive

--- src/laboratoryWorkString/util/DemoStrings.java
+++ src/laboratoryWorkString/util/DemoStrings.java
@@ -2,6 +2,6 @@ package laboratoryWorkString.util;
 
 public class DemoStrings {
         public static String[] testLinesArray = {"Hello my black kitty! I love my BlAcK kitty!",
-                "I blackBlack 123 have 2 BLACK kitty 4 and 1 blackaged dog and 23 days!", "I want     to stay  junior java    dev!",
+                "I blackBlack 123 have 2 BLACK kitty 4 and 1 blackaged dog and 23 days!     ", "I want     to stay  junior java    dev!",
                 "I want to a starting salary of 500 dollars!", "ProgramingIsCool"};
 }
