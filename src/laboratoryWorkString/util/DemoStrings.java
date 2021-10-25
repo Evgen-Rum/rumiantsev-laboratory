c207e00271481c60061c502d788c8960eba74537
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-10-25 19:57:26 +0300

    minor fixes

--- src/laboratoryWorkString/util/DemoStrings.java
+++ src/laboratoryWorkString/util/DemoStrings.java
@@ -3,7 +3,7 @@ package laboratoryWorkString.util;
 public class DemoStrings {
         public static String[] testLinesArray = {"1) Hello my black kitty! I love my BlAcK kitty!",
                 "2) I blackBlack 123 have 2 BLACK kitty 4 and 1 blackaged dog and 23 days!     ",
-                "3) I want to stay  junior java    developer!",
+                "3) I want to stay  junior    java    developer!",
                 "4) I want to a starting salary of 500 dollars!", "5) ProgramingIsCool",
         "6) What                    about your knowledgeOf theString class?        black"};
 }
