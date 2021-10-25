dc6a10f7ee3031555a6973cdd22350321d2f2020
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-10-25 15:58:31 +0300

    removed extra spaces in lines

--- src/laboratoryWorkString/Task5.java
+++ src/laboratoryWorkString/Task5.java
@@ -1,11 +1,14 @@
 package laboratoryWorkString;
+/*
+Удалить в строке все лишние пробелы.
+ */
 
 import laboratoryWorkString.util.DemoStrings;
 
 public class Task5 {
     public static void main(String[] args) {
         for (int i = 0; i < DemoStrings.testLinesArray.length; i++) {
-
+        System.out.println(DemoStrings.testLinesArray[i].trim().replaceAll("\\s{2,}", " "));
         }
     }
 }
