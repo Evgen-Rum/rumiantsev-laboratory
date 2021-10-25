afb02d9ec0c9628e0688696ac1d5aeca47c3258e
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-10-25 16:05:28 +0300

    Added a new element to the array

--- src/laboratoryWorkString/Task6.java
+++ src/laboratoryWorkString/Task6.java
@@ -1,7 +1,10 @@
 package laboratoryWorkString;
 /*
-
+Найдите наибольшее количество пробелов идущих подряд в тексте.
  */
 
 public class Task6 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/laboratoryWorkString/util/DemoStrings.java
+++ src/laboratoryWorkString/util/DemoStrings.java
@@ -1,7 +1,9 @@
 package laboratoryWorkString.util;
 
 public class DemoStrings {
-        public static String[] testLinesArray = {"Hello my black kitty! I love my BlAcK kitty!",
-                "I blackBlack 123 have 2 BLACK kitty 4 and 1 blackaged dog and 23 days!     ", "I want     to stay  junior java    dev!",
-                "I want to a starting salary of 500 dollars!", "ProgramingIsCool"};
+        public static String[] testLinesArray = {"1) Hello my black kitty! I love my BlAcK kitty!",
+                "2) I blackBlack 123 have 2 BLACK kitty 4 and 1 blackaged dog and 23 days!     ",
+                "3) I want to stay  junior java    dev!",
+                "4) I want to a starting salary of 500 dollars!", "5) ProgramingIsCool",
+        "6) What         about your knowledgeOf theString class?        black"};
 }
